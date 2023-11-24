27a88c357dba48567b4c2e9969c7cf26084e60d1
Juan <elranchodejuancho12@gmail.com>
2023-11-24 14:17:52 +0100

    primeros cambios

--- App.java
+++ App.java
@@ -4,6 +4,9 @@ public class App {
     System.out.println("Contenido 2a");
     System.out.println("Contenido 3");
     System.out.println("Contenido 2b");
+    System.out.println("Juan Blas Martínez");
+    System.out.println("IES San Andrés");
+    System.out.println("Residencia Doña Sancha");
   }
   
 }
